a6423f5093d37bd73c4ff33baaeff7a36f76b254
Torok Balint <torokbalinz@gmail.com>
2026-07-18 20:46:51 +0300

    Piaci mentés gomb működik

--- js/uiManager.js
+++ js/uiManager.js
@@ -132,3 +132,25 @@ setTimeout(() => {
 
 
 }
+
+UIManager.init();
+
+document.getElementById("btnSaveStatistics").onclick = () => {
+
+    if (!confirm("Biztosan szeretnél egy piaci pillanatképet menteni?")) {
+        return;
+    }
+
+    fetch("/api/statistics/save", {
+        method: "POST"
+    })
+    .then(r => r.json())
+    .then(() => {
+        alert("Piaci pillanatkép sikeresen elmentve!");
+    })
+    .catch(err => {
+        console.error(err);
+        alert("Hiba történt a mentés során.");
+    });
+
+};
