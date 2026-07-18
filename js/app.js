0c3bff60e96bd7f5e202f2636b2f88bacce9d415
Torok Balint <torokbalinz@gmail.com>
2026-07-18 20:57:00 +0300

    Statistics button

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     console.log("IngatlanPro indult");
-    
+
     UIManager.initMenu();
 
     DataManager.init();
 
     FilterManager.init();
-    
+
     NewPropertyMap.init();
-    
+
     NewPropertyManager.init();
+
+    document.getElementById("btnSaveStatistics").onclick = () => {
+
+        if (!confirm("Biztosan szeretnél egy piaci pillanatképet menteni?")) {
+            return;
+        }
+
+        fetch("/api/statistics/save", {
+            method: "POST"
+        })
+        .then(r => r.json())
+        .then(() => {
+            alert("Piaci pillanatkép sikeresen elmentve!");
+        })
+        .catch(err => {
+            console.error(err);
+            alert("Hiba történt a mentés során.");
+        });
+
+    };
+
 });
