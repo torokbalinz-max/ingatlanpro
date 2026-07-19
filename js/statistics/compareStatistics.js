b1fb4c1b57bc5541f7fdc8e0e6c86a8b5712a298
Torok Balint <torokbalinz@gmail.com>
2026-07-19 18:04:44 +0300

    Snapshot details API

--- js/statistics/compareStatistics.js
+++ js/statistics/compareStatistics.js
@@ -106,10 +106,103 @@ class CompareStatistics {
 
     }
 
-    static compare(){
+    static compare() {
 
-        alert("Következő lépésben itt fogjuk kiszámolni a különbségeket.");
+    const from =
+        document.getElementById("compareFrom").value;
 
-    }
+    const to =
+        document.getElementById("compareTo").value;
+
+    Promise.all([
+
+        fetch("/api/statistics/" + from).then(r => r.json()),
+
+        fetch("/api/statistics/" + to).then(r => r.json())
+
+    ])
+
+    .then(([oldData, newData]) => {
+
+        const oldStat = oldData.snapshot;
+        const newStat = newData.snapshot;
+
+        const arValtozas =
+            ((newStat.avg_price - oldStat.avg_price)
+            / oldStat.avg_price * 100);
+
+        const nmValtozas =
+            ((newStat.avg_price_nm - oldStat.avg_price_nm)
+            / oldStat.avg_price_nm * 100);
+
+        let html = `
+
+            <div class="statisticsGrid">
+
+                <div class="statCard">
+
+                    <h3>Átlag ár</h3>
+
+                    <h2>
+                        ${Math.round(oldStat.avg_price).toLocaleString()} €
+                    </h2>
+
+                    <h1>
+
+                        ➜
+
+                    </h1>
+
+                    <h2>
+                        ${Math.round(newStat.avg_price).toLocaleString()} €
+                    </h2>
+
+                    <hr>
+
+                    <h2>
+
+                        ${arValtozas.toFixed(1)} %
+
+                    </h2>
+
+                </div>
+
+                <div class="statCard">
+
+                    <h3>Átlag €/m²</h3>
+
+                    <h2>
+                        ${Math.round(oldStat.avg_price_nm)}
+                    </h2>
+
+                    <h1>
+
+                        ➜
+
+                    </h1>
+
+                    <h2>
+                        ${Math.round(newStat.avg_price_nm)}
+                    </h2>
+
+                    <hr>
+
+                    <h2>
+
+                        ${nmValtozas.toFixed(1)} %
+
+                    </h2>
+
+                </div>
+
+            </div>
+
+        `;
+
+        document.getElementById("compareResult").innerHTML = html;
+
+    });
+
+}
 
 }
