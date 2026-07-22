7402ee17a04440cbd0708d408a1d9a740c36d440
Torok Balint <torokbalinz@gmail.com>
2026-07-22 14:21:12 +0300

    Fix StatisticsManager

--- js/statistics/chartStatistics.js
+++ js/statistics/chartStatistics.js
@@ -6,14 +6,9 @@ class ChartStatistics {
 
     static destroy() {
 
-        if (ChartStatistics.chart1)
-            ChartStatistics.chart1.destroy();
-
-        if (ChartStatistics.chart2)
-            ChartStatistics.chart2.destroy();
-
-        if (ChartStatistics.chart3)
-            ChartStatistics.chart3.destroy();
+        if (ChartStatistics.chart1) ChartStatistics.chart1.destroy();
+        if (ChartStatistics.chart2) ChartStatistics.chart2.destroy();
+        if (ChartStatistics.chart3) ChartStatistics.chart3.destroy();
 
     }
 
@@ -21,40 +16,126 @@ class ChartStatistics {
 
         this.destroy();
 
-        // ===== ÁLLAPOT =====
+        // ================= ÁLLAPOT =================
 
         const allapotok = {};
 
         lista.forEach(i => {
 
             const nev = i.allapot || "Ismeretlen";
-
             allapotok[nev] = (allapotok[nev] || 0) + 1;
 
         });
 
         ChartStatistics.chart1 = new Chart(
-
             document.getElementById("stateChart"),
-
             {
-
-                type: "pie",
-
+                type: "doughnut",
                 data: {
-
                     labels: Object.keys(allapotok),
-
                     datasets: [{
-
                         data: Object.values(allapotok)
-
                     }]
-
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: "bottom"
+                        },
+                        title: {
+                            display: true,
+                            text: "Állapot szerinti megoszlás"
+                        }
+                    }
                 }
-
             }
+        );
 
+        // ================= SZOBÁK =================
+
+        const szobak = {};
+
+        lista.forEach(i => {
+
+            const nev = i.szobak + " szoba";
+            szobak[nev] = (szobak[nev] || 0) + 1;
+
+        });
+
+        ChartStatistics.chart2 = new Chart(
+            document.getElementById("roomChart"),
+            {
+                type: "bar",
+                data: {
+                    labels: Object.keys(szobak),
+                    datasets: [{
+                        label: "Ingatlanok",
+                        data: Object.values(szobak)
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        title: {
+                            display: true,
+                            text: "Szobaszám szerinti megoszlás"
+                        }
+                    }
+                }
+            }
+        );
+
+        // ================= EMELET =================
+
+        const emeletek = {};
+
+        lista.forEach(i => {
+
+            let nev;
+
+            const e = parseInt(i.emelet);
+
+            if (isNaN(e))
+                nev = "Ismeretlen";
+            else if (e <= 0)
+                nev = "Földszint";
+            else if (e >= 4)
+                nev = "4+";
+            else
+                nev = e + ".";
+
+            emeletek[nev] = (emeletek[nev] || 0) + 1;
+
+        });
+
+        ChartStatistics.chart3 = new Chart(
+            document.getElementById("floorChart"),
+            {
+                type: "bar",
+                data: {
+                    labels: Object.keys(emeletek),
+                    datasets: [{
+                        label: "Ingatlanok",
+                        data: Object.values(emeletek)
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        title: {
+                            display: true,
+                            text: "Emelet szerinti megoszlás"
+                        }
+                    }
+                }
+            }
         );
 
     }
